099b9e872b3bec1f9144ed234238cb8742d162d2
marian122 <maleksiev12@gmail.com>
2020-07-20 15:03:56 +0300

    Add project files.

---
+++ RedVsGreen/Generation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedVsGreen
+{
+    public class Generation
+    {
+        public Generation(int x, int y, int[][] matrix, int targetX, int targetY, int totalGenerations)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Matrix = matrix;
+            this.TargetX = targetX;
+            this.TargetY = targetY;
+            this.TotalGenerations = totalGenerations;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public int[][] Matrix { get; }
+        public int TargetX { get; }
+        public int TargetY { get; }
+        public int TotalGenerations { get; }
+
+        public void Calculate()
+        {
+            var matrixForNextGeneration = new List<string>();
+            var counter = 0;
+
+            for (int i = 0; i <= this.TotalGenerations; i++)
+            {
+                foreach (var data in matrixForNextGeneration)
+                {
+                    var splittedData = data.Split(" ").Select(int.Parse).ToArray();
+                    var row = splittedData[0];
+                    var col = splittedData[1];
+                    var value = splittedData[2];
+                    this.Matrix[row][col] = value;
+                }
+                matrixForNextGeneration.Clear();
+
+                if (this.Matrix[TargetX][TargetY] == 1)
+                {
+                    counter++;
+                }
+
+                for (int row = 0; row < this.X; row++)
+                {
+                    for (int col = 0; col < this.Y; col++)
+                    {
+                        int greenNeighbours = CheckForGreen(this.Matrix, row, col);
+                        if (this.Matrix[row][col] == 1)
+                        {
+                            if (greenNeighbours != 2 && greenNeighbours != 3 && greenNeighbours != 6)
+                            {
+                                matrixForNextGeneration.Add($"{row} {col} 0");
+                            }
+                        }
+                        else
+                        {
+                            if (greenNeighbours == 3 || greenNeighbours == 6)
+                            {
+                                matrixForNextGeneration.Add($"{row} {col} 1");
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+
+        private int CheckForGreen(int[][] matrix, int row, int col)
+        {
+            int countOfRows = matrix.Length;
+            int countOfCols = matrix[0].Length;
+            int greenNeighboors = 0;
+            if (col + 1 < countOfCols && matrix[row][col + 1] == 1)
+            {
+                greenNeighboors++;
+            }
+            if (col - 1 >= 0 && matrix[row][col - 1] == 1)
+            {
+                greenNeighboors++;
+            }
+            if (row - 1 >= 0 && matrix[row - 1][col] == 1)
+            {
+                greenNeighboors++;
+            }
+            if (row + 1 < countOfRows && matrix[row + 1][col] == 1)
+            {
+                greenNeighboors++;
+            }
+            if ((row - 1 >= 0 && col - 1 >= 0) && matrix[row - 1][col - 1] == 1)
+            {
+                greenNeighboors++;
+            }
+            if ((row + 1 < countOfRows && col + 1 < countOfCols) && matrix[row + 1][col + 1] == 1)
+            {
+                greenNeighboors++;
+            }
+            if ((row - 1 >= 0 && col + 1 < countOfCols) && matrix[row - 1][col + 1] == 1)
+            {
+                greenNeighboors++;
+            }
+            if ((row + 1 < countOfRows && col - 1 >= 0) && matrix[row + 1][col - 1] == 1)
+            {
+                greenNeighboors++;
+            }
+            return greenNeighboors;
+        }
+
+    }
+}
---
+++ RedVsGreen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace RedVsGreen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // First example.
+            // 3, 3
+            // 000
+            // 111
+            // 000
+            // 1, 0, 10
+            // Expected result = 5, but i got 6. The solutions seems true for me.
+
+            // Second example.
+            // 4, 4
+            // 1001
+            // 1111
+            // 0100
+            // 1010
+            // 2, 2, 15
+            // Expected result = 14, my result = 14.
+
+
+            var size = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int countOfRows = size[0];
+            int countOfCols = size[1];
+            int[][] matrix = new int[countOfRows][];
+
+            for (int row = 0; row < countOfRows; row++)
+            {
+                matrix[row] = new int[countOfCols];
+                matrix[row] = Console.ReadLine().Select(x => (int)char.GetNumericValue(x)).ToArray();
+            }
+
+            var lastArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var targetX = lastArgs[0];
+            var targetY = lastArgs[1];
+            var totalGenerations = lastArgs[2];
+
+            var generation = new Generation(countOfRows, countOfCols, matrix, targetX, targetY, totalGenerations);
+
+            generation.Calculate();
+        }
+    }
+}
